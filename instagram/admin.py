cf80a12aa4eed16016ebefbe6dd4693a79ba5425
hizen <junwoo1357@gmail.com>
2023-05-09 20:56:42 +0900

    강의 2-3 장고 admin을 통한 데이터 관리(2-2는 수업만 진행 하였으므로 패스)

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+# 첫번째 방법
+# 바로 사용
+# admin.site.register(Post)
+
+# 두번 째 방법
+# 클래스에 담아서 사용
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+#
+# admin.site.register(Post, PostAdmin)
+
+# 세번 째 방법
+# 데코레이터 이용
+@admin.register(Post) # Wrapping
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'is_public', 'created_at', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message']
+    def message_length(self, post):
+        return f'{len(post.message)} 글자'
--- instagram/models.py
+++ instagram/models.py
@@ -3,5 +3,19 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Java toString
+    def __str__(self):
+        #return f"Custome Post Object({self.id})"
+        return f'{self.message}'
+
+    # model에 메세지 관련 특성 구현 하는 방법(admin 에서도 구현 가능)
+    # def message_length(self):
+    #     return len(self.message)
+    #
+    # message_length.short_description = '메세지 글자수'
+    #
+    #
