d9e41affaadacb2c40ffde4c6e2a3fb610b0c121
JunWoo Lee <junwoo1357@gmail.com>
2023-05-22 20:37:07 +0900

    필기 추가
     - 필기내용 추가

--- instagram/views.py
+++ instagram/views.py
@@ -6,9 +6,10 @@ def post_list(request):
     q = request.GET.get('q', '')
     if q:
         qs = qs.filter(message__icontains=q)
-    # instagram/templates/instagram/post_list.html
+    # instagram/templates/instagram/post_list.html instagram/templates 까지는 고정된 주소
+    # render 로 html 파일로 보내줌
     return render(request, 'instagram/post_list.html', {
-        'post_list1' : qs,
+        'post_list1' : qs, # template 에서 사용할 이름 지정
         'q' : q
     }
                   )
